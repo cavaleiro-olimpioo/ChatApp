c3ee5e9c18c1e123e38a4fd63ddccce8cf9380e2
Guilherme Olimpio <guilhermearnaldi@gmail.com>
2026-08-29 13:18:35 -0300

    Beta

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -1,7 +1,13 @@
 public class Program
 {
+    private String username;
     public static void main(String[] args)
     {
+        Console.Write("Enter your username: ");
+        username = Console.ReadLine();
+
+        Console.WriteLine("-=-=-=-=-= CHATAPP - Welcome, " + username + "! =-=-=-=-=-");
+
         
     }
 }
