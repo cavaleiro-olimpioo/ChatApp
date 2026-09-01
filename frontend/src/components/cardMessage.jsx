b1a8605c8a5abf3627b3bdebfd50f72dfa222f34
Guilherme Olimpio <guilhermearnaldi@gmail.com>
2026-08-31 22:06:36 -0300

    fixCardUI

--- frontend/src/components/cardMessage.jsx
+++ frontend/src/components/cardMessage.jsx
@@ -7,7 +7,7 @@ function cardMessage({ message, userMsg, user }) {
         );
     } else {
         return (
-            <div className='bg-indigo-800 ml-3 mt-5 mb-5 max-w-5/6 p-3 rounded-sm'>
+            <div className='bg-indigo-800 ml-3 mt-5 mb-5 w-fit max-w-5/6 p-3 rounded-sm'>
                 <h1 className='font-bold text-gray-100 text-md mb-2'>{userMsg}</h1>
                 <p className='text-gray-100'>{message}</p>
             </div>
